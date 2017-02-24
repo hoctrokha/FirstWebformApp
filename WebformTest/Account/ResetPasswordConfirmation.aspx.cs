ca10dbd3bf9b9a6f16e2297a4fb9f75441443886
Sy Le Kha <hoctrokha@yahoo.com>
2017-02-24 11:23:40 +0700

    Initial version

---
+++ WebformTest/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebformTest.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ WebformTest/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebformTest.Startup))]
+namespace WebformTest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
